96e8a17f5e301ce6ebfb03f5b9dd916b1797ec5e
yanchim <yanchi.x@foxmail.com>
2023-06-29 14:12:01 +0800

    Add snippets

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -19,27 +19,35 @@
     (setq package-archives
           `(
 
-            ;; ;; official
+            ;; ;; Official.
             ;; ,(cons "gnu"    (concat proto "://elpa.gnu.org/packages/"))
             ;; ,(cons "nongnu" (concat proto "://elpa.nongnu.org/nongnu/"))
+            ;; ,(cons "gnu-devel" (concat proto "://elpa.gnu.org/devel/"))
+            ;; ,(cons "nongnu-devel" (concat proto "://elpa.nongnu.org/nongnu-devel/"))
             ;; ,(cons "melpa"  (concat proto "://melpa.org/packages/"))
             ;; ;; ,(cons "melpa-stable" (concat proto "://stable.melpa.org/packages/"))
 
-            ;; ;; emacs-china
+            ;; ;; Emacs-china.
             ;; ,(cons "gnu"    (concat proto "://elpa.emacs-china.org/gnu/"))
             ;; ,(cons "nongnu" (concat proto "://elpa.emacs-china.org/nongnu/"))
+            ;; ,(cons "gnu-devel" (concat proto "://elpa.emacs-china.org/gnu-devel/"))
+            ;; ,(cons "nongnu-devel" (concat proto "://elpa.emacs-china.org/nongnu-devel/"))
             ;; ,(cons "melpa"  (concat proto "://elpa.emacs-china.org/melpa/"))
             ;; ;; ,(cons "melpa-stable" (concat proto "://elpa.emacs-china.org/stable-melpa/"))
 
-            ;; ;; 163
+            ;; ;; 163.
             ;; ,(cons "gnu"    (concat proto "://mirrors.163.com/elpa/gnu/"))
             ;; ,(cons "nongnu" (concat proto "://mirrors.163.com/elpa/nongnu/"))
+            ;; ,(cons "gnu-devel" (concat proto "://mirrors.163.com/elpa/gnu-devel/"))
+            ;; ,(cons "nongnu-devel" (concat proto "://mirrors.163.com/elpa/nongnu-devel/"))
             ;; ,(cons "melpa"  (concat proto "://mirrors.163.com/elpa/melpa/"))
             ;; ;; ,(cons "melpa-stable" (concat proto "://mirrors.163.com/elpa/stable-melpa/"))
 
-            ;; tuna
+            ;; Tuna.
             ,(cons "gnu"    (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/"))
             ,(cons "nongnu" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/"))
+            ,(cons "gnu-devel" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/gnu-devel/"))
+            ,(cons "nongnu-devel" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu-devel/"))
             ,(cons "melpa"  (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"))
             ;; ,(cons "melpa-stable" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/stable-melpa/"))
 
