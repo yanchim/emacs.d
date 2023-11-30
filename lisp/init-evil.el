1d64ee19cc67589b7fa630fe6ca7cd87b70b6db5
yanchim <yanchi.x@foxmail.com>
2023-11-30 23:13:31 +0800

    Use evil's leader key to replace general.el

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,4 +1,4 @@
-;;; ~/.emacs.d/etc/init-evil.el --- evil configuration -*- lexical-binding: t; -*-
+;;; init-evil.el --- evil configuration -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 ;;
@@ -16,11 +16,143 @@
                         "Toggle `evil-state' based on current state."
                         (if (eq evil-state 'normal)
                             (evil-emacs-state)
-                          (evil-normal-state))))
-         (org-capture-mode . (lambda ()
-                               "Minor modes with Evil Emacs state."
-                               (evil-emacs-state))))
-  :bind ((:map evil-normal-state-map
+                          (evil-normal-state)))))
+  :bind (;; Evil leader key bindings.
+;;;; Evil leader.
+         ("<leader><SPC>" . execute-extended-command)
+         ("<leader>;" . eval-expression)
+         ("<leader>cc" . compile)
+         ("<leader>cp" . project-compile)
+         ("<leader>cr" . recompile)
+;;;;; Evil leader file.
+         ("<leader>ff" . find-file)
+         ("<leader>fc" . my-copy-file-name)
+         ("<leader>fd" . my-delete-this-file)
+         ("<leader>fo" . my-open-file-externally)
+         ("<leader>fr" . my-rename-this-file)
+         ("<leader>f/" . find-file-other-window)
+;;;;; Evil leader git.
+         ("<leader>gd" . magit-dispatch)
+         ("<leader>gg" . magit-status)
+         ("<leader>gf" . magit-file-dispatch)
+         ("<leader>gs" . consult-git-grep)
+;;;;; Evil leader lsp.
+         ("<leader>ll" . eglot)
+         ("<leader>la" . eglot-code-actions)
+         ("<leader>lc" . eglot-show-workspace-configuration)
+         ("<leader>lf" . eglot-format)
+         ("<leader>lq" . eglot-shutdown)
+         ("<leader>lQ" . eglot-shutdown-all)
+         ("<leader>lr" . eglot-rename)
+;;;;; Evil leader my custom.
+         ("<leader>mc" . my-avy-copy-thing-at-point)
+         ("<leader>mD" . my-delete-visual-blank-lines)
+         ("<leader>md" . my-delete-blank-lines)
+         ("<leader>mF" . my-load-font)
+         ("<leader>mf" . my-load-default-font)
+         ("<leader>mp" . my-pangu-spacing-current-buffer)
+         ("<leader>mT" . my-load-default-theme)
+         ("<leader>mt" . my-load-theme)
+         ("<leader>mx" . execute-extended-command)
+;;;;; Evil leader org.
+         ("<leader>oa" . org-agenda)
+         ("<leader>ob" . org-switchb)
+         ("<leader>oc" . org-capture)
+         ("<leader>ot" . org-toggle-link-display)
+;;;;; Evil leader search.
+         ("<leader>sf" . consult-fd)
+         ("<leader>sF" . consult-find)
+         ("<leader>sg" . consult-grep)
+         ("<leader>sG" . consult-git-grep)
+         ("<leader>so" . my-search-online)
+         ("<leader>sr" . consult-ripgrep)
+         ("<leader>ss" . consult-line)
+         ("<leader>su" . consult-focus-lines)
+         ("<leader>sk" . consult-keep-lines)
+         ("<leader>si" . imenu)
+;;;;; Evil leader toggle.
+         ("<leader>tA"  . abbrev-mode)
+         ("<leader>ta"  . auto-fill-mode)
+         ("<leader>tD"  . darkroom-mode)
+         ("<leader>td"  . darkroom-tentative-mode)
+         ("<leader>tff" . toggle-frame-fullscreen)
+         ("<leader>tfm" . toggle-frame-maximized)
+         ("<leader>tg"  . glasses-mode)
+         ("<leader>th"  . global-hl-line-mode)
+         ("<leader>ti"  . display-fill-column-indicator-mode)
+         ("<leader>tj"  . toggle-truncate-lines)
+         ("<leader>tk"  . visual-line-mode)
+         ("<leader>tl"  . display-line-numbers-mode)
+         ("<leader>ts"  . subword-mode)
+         ("<leader>tt"  . load-theme)
+         ("<leader>tv"  . view-mode)
+         ("<leader>tw"  . whitespace-mode)
+;;;; Evil localleader.
+         ("<localleader>,"  . execute-extended-command)
+         ("<localleader>."  . evil-ex)
+         ("<localleader>;"  . eval-expression)
+         ("<localleader>aa" . avy-goto-char-2)
+         ("<localleader>ac" . avy-goto-char-timer)
+         ("<localleader>ae" . avy-goto-end-of-line)
+         ("<localleader>af" . beginning-of-defun)
+         ("<localleader>ag" . avy-goto-line)
+         ("<localleader>aj" . avy-goto-line-below)
+         ("<localleader>ak" . avy-goto-line-above)
+         ("<localleader>ar" . align-regexp)
+         ("<localleader>aw" . avy-goto-word-or-subword-1)
+         ("<localleader>bb" . (lambda () (interactive) (switch-to-buffer nil)))
+         ("<localleader>cc" . evilnc-comment-or-uncomment-lines)
+         ("<localleader>cd" . evilnc-copy-and-comment-lines)
+         ("<localleader>cl" . evilnc-quick-comment-or-uncomment-to-the-line)
+         ("<localleader>cp" . evilnc-comment-or-uncomment-paragraphs)
+         ("<localleader>cr" . comment-or-uncomment-region)
+         ("<localleader>dd" . pwd)
+         ("<localleader>dj" . dired-jump)
+         ("<localleader>dp" . project-dired)
+         ("<localleader>ee" . eval-expression)
+         ("<localleader>el" . eval-last-sexp)
+         ("<localleader>f/" . find-file-other-window)
+         ("<localleader>ff" . find-file)
+         ("<localleader>gg" . switch-to-buffer)
+         ("<localleader>hh" . mark-whole-buffer)
+         ("<localleader>kk" . my-kill-other-buffers-without-special-ones)
+         ("<localleader>ne" . flymake-goto-next-error)
+         ("<localleader>pe" . flymake-goto-prev-error)
+         ("<localleader>sc" . shell-command)
+         ("<localleader>sf" . consult-fd)
+         ("<localleader>sr" . consult-ripgrep)
+         ("<localleader>ss" . consult-line)
+         ("<localleader>si" . imenu)
+         ("<localleader>xb" . switch-to-buffer)
+         ("<localleader>xf" . find-file)
+         ("<localleader>xk" . kill-buffer)
+         ("<localleader>xs" . save-buffer)
+         ("<localleader>xx" . kill-buffer)
+         ("<localleader>vn" . diff-hl-next-hunk)
+         ("<localleader>vp" . diff-hl-previous-hunk)
+;;;;; Evil localleader window.
+         ("<localleader>0"  . winum-select-window-0-or-10)
+         ("<localleader>1"  . winum-select-window-1)
+         ("<localleader>2"  . winum-select-window-2)
+         ("<localleader>3"  . winum-select-window-3)
+         ("<localleader>4"  . winum-select-window-4)
+         ("<localleader>5"  . winum-select-window-5)
+         ("<localleader>6"  . winum-select-window-6)
+         ("<localleader>7"  . winum-select-window-7)
+         ("<localleader>8"  . winum-select-window-8)
+         ("<localleader>9"  . winum-select-window-9)
+         ("<localleader>oo" . my-toggle-full-window)
+         ("<localleader>sa" . split-window-vertically)
+         ("<localleader>sd" . split-window-horizontally)
+         ("<localleader>sh" . split-window-below)
+         ("<localleader>sq" . delete-window)
+         ("<localleader>sv" . split-window-right)
+         ("<localleader>rr" . my-rotate-windows)
+         ("<localleader>tt" . my-toggle-two-split-window)
+         ("<localleader>xo" . ace-window)
+         ("<localleader>ws" . ace-swap-window)
+;;;; Evil state bindings.
+         (:map evil-normal-state-map
                ("]b" . next-buffer)
                ("[b" . previous-buffer)
                ("g1" . avy-goto-char-timer)
@@ -31,8 +163,6 @@
                ("glk" . avy-goto-line-above)
                ("gle" . avy-goto-end-of-line)
                ("M-." . xref-find-definitions))
-         (:map evil-visual-state-map
-               ("v" . expreg-expand))
          (:map evil-command-line-map
                ("C-a" . move-beginning-of-line)
                ("C-e" . move-end-of-line)
@@ -40,7 +170,10 @@
                ("C-b" . backward-char)
                ("C-d" . delete-char)
                ("C-k" . kill-line)
-               ("C-o" . evil-command-window)))
+               ("C-o" . evil-command-window))
+         (:map evil-visual-state-map
+               ("V" . expreg-contract)
+               ("v" . expreg-expand)))
   :custom
   (evil-ex-interactive-search-highlight 'selected-window)
   ;; Move back the cursor one position when exiting insert mode.
@@ -49,9 +182,18 @@
   (evil-move-beyond-eol t)
   ;; Make evil search like vim.
   (evil-ex-search-vim-style-regexp t)
+  ;; Use native undo commands.
+  (evil-undo-system 'undo-redo)
   :config
   ;; Make evil-search behave more like VIM.
   (evil-select-search-module 'evil-search-module 'evil-search)
+
+  ;; Leader key.
+  (let ((state '(normal visual motion)))
+    ;; SPACE as leader.
+    (evil-set-leader state (kbd "SPC"))
+    ;; Comma as localleader.
+    (evil-set-leader state  "," t))
 
   (defun evil-unimpaired-insert-newline-above (count)
     "Insert COUNT blank line(s) above current line."
@@ -75,18 +217,18 @@
 
   (advice-add 'keyboard-quit :before #'my--evil-disable-ex-highlight)
 
-  ;; http://blog.binchen.org/posts/code-faster-by-extending-emacs-evil-text-object
   (defun my--evil-paren-range (count beg end type inclusive)
     "Get minimum range of paren text object.
+
 COUNT, BEG, END, TYPE is used to identify the text object.
-If INCLUSIVE is t, the text object is inclusive."
-    (let ((parens
-           '("()" "[]" "{}" "<>" "\"\"" "''" "``"
-             "（）" "《》" "「」" "『』" "【】" "〖〗"
-             "“”" "‘’" "［］" "〔〕" "｛｝"))
+If INCLUSIVE is t, the text object is inclusive.
+
+URL `http://blog.binchen.org/posts/code-faster-by-extending-emacs-evil-text-object'."
+    (let ((parens '("()" "[]" "{}" "<>" "\"\"" "''" "``"
+                    "（）" "《》" "「」" "『』" "【】" "〖〗"
+                    "“”" "‘’" "［］" "〔〕" "｛｝"))
           (pos (point))
-          range
-          found-range)
+          range found-range)
       (dolist (paren parens)
         (condition-case _
             (let ((char1 (aref paren 0))
@@ -246,207 +388,6 @@ URL `https://stackoverflow.com/a/22418983/4921402'."
                ("gc" . evilnc-comment-operator)
                ("gp" . evilnc-copy-and-comment-operator)
                ("gy" . evilnc-yank-and-comment-operator))))
-
-(use-package general
-  :config (general-evil-setup)
-
-  (general-create-definer my-comma-leader-def
-    :prefix ","
-    :states '(normal visual))
-
-  (my-comma-leader-def
-    ","  #'execute-extended-command
-    "."  #'evil-ex
-    ";"  #'eval-expression
-    "aa" #'avy-goto-char-2
-    "ac" #'avy-goto-char-timer
-    "ag" #'avy-goto-line
-    "ae" #'avy-goto-end-of-line
-    "aj" #'avy-goto-line-below
-    "ak" #'avy-goto-line-above
-    "af" #'beginning-of-defun
-    "ar" #'align-regexp
-    "aw" #'avy-goto-word-or-subword-1
-    "bb" (lambda ()
-           (interactive) (switch-to-buffer nil))
-    "cc" #'evilnc-comment-or-uncomment-lines
-    "cd" #'evilnc-copy-and-comment-lines
-    "cl" #'evilnc-quick-comment-or-uncomment-to-the-line
-    "cp" #'evilnc-comment-or-uncomment-paragraphs
-    "cr" #'comment-or-uncomment-region
-    "si" #'imenu
-    "dd" #'pwd
-    "dj" #'dired-jump
-    "dp" #'project-dired
-    "ee" #'eval-expression
-    "el" #'eval-last-sexp
-    "ff" #'find-file
-    "f/" #'find-file-other-window
-    "gg" #'switch-to-buffer
-    "hh" #'mark-whole-buffer
-    "kk" #'my-kill-other-buffers-without-special-ones
-    "sc" #'shell-command
-    "xx" #'kill-buffer
-    ;; Keymaps in `ctl-x-map'.
-    "xb" #'switch-to-buffer
-    "xf" #'find-file
-    "xk" #'kill-buffer
-    "xs" #'save-buffer
-    ;; Search.
-    "ss" #'consult-line
-    "sr" #'consult-ripgrep
-    "sf" #'consult-fd
-    ;; Window.
-    "0"  #'winum-select-window-0-or-10
-    "1"  #'winum-select-window-1
-    "2"  #'winum-select-window-2
-    "3"  #'winum-select-window-3
-    "4"  #'winum-select-window-4
-    "5"  #'winum-select-window-5
-    "6"  #'winum-select-window-6
-    "7"  #'winum-select-window-7
-    "8"  #'winum-select-window-8
-    "9"  #'winum-select-window-9
-    "oo" #'my-toggle-full-window
-    "sa" #'split-window-vertically
-    "sd" #'split-window-horizontally
-    "sh" #'split-window-below
-    "sq" #'delete-window
-    "sv" #'split-window-right
-    "rr" #'my-rotate-windows
-    "tt" #'my-toggle-two-split-window
-    "xo" #'ace-window
-    "ws" #'ace-swap-window
-    ;; Check.
-    "fa" #'flyspell-auto-correct-word
-    "fm" #'flymake-mode
-    "fn" #'flyspell-goto-next-error
-    "fs" #'flyspell-mode
-    "ne" #'flymake-goto-next-error
-    "pe" #'flymake-goto-prev-error
-    ;; Version control.
-    "va" #'vc-next-action
-    "vg" #'vc-annotate
-    "vn" #'diff-hl-next-hunk
-    "vp" #'diff-hl-previous-hunk
-    "vu" #'vc-revert)
-
-  (general-create-definer my-space-leader-def
-    :prefix "SPC"
-    :states '(normal visual))
-
-  (my-space-leader-def
-    "SPC" #'execute-extended-command
-    ";"   #'eval-expression
-    ;; Bookmark/buffer.
-    "b"  #'(:ignore t)
-    "bb" #'switch-to-buffer
-    "b/" #'switch-to-buffer-other-window
-    "bd" #'bookmark-delete
-    "be" #'eval-buffer
-    "bj" #'bookmark-jump
-    "bJ" #'bookmark-jump-other-window
-    "bk" #'kill-buffer
-    "bl" #'bookmark-bmenu-list
-    "bm" #'bookmark-set
-    "bo" #'my-kill-other-buffers-without-special-ones
-    "bO" #'my-kill-other-buffers-with-special-ones
-    "bs" #'bookmark-save
-    "bx" #'my-switch-scratch-buffer
-    ;; Code.
-    "c"  #'(:ignore t)
-    "ck" #'compile
-    "cr" #'recompile
-    ;; Dired.
-    "dj" #'dired-jump
-    "d/" #'dired-jump-other-window
-    "dd" #'pwd
-    ;; File.
-    "f"  #'(:ignore t)
-    "fb" #'my-browse-this-file
-    "ff" #'find-file
-    "f/" #'find-file-other-window
-    "fc" #'my-copy-file-name
-    "fd" #'my-delete-this-file
-    "fD" #'my-delete-file
-    "fo" #'my-open-file-externally
-    "fr" #'my-rename-this-file
-    "fs" #'my-sudo-edit-file
-    "fS" #'my-sudo-find-file
-    ;; Git.
-    "g"  #'(:ignore t)
-    "gd" #'magit-dispatch
-    "gg" #'magit-status
-    "gf" #'magit-file-dispatch
-    "gs" #'consult-git-grep
-    ;; Lsp.
-    "l"  #'(:ignore t)
-    "ll" #'eglot
-    "la" #'eglot-code-actions
-    "lc" #'eglot-show-workspace-configuration
-    "lf" #'eglot-format
-    "lq" #'eglot-shutdown
-    "lQ" #'eglot-shutdown-all
-    "lr" #'eglot-rename
-    ;; My custom.
-    "m" #'(:ignore t)
-    "mc" #'my-avy-copy-thing-at-point
-    "mF" #'my-load-font
-    "mf" #'my-load-default-font
-    "mp" #'my-pangu-spacing-current-buffer
-    "mD" #'my-delete-visual-blank-lines
-    "md" #'my-delete-blank-lines
-    "mi" #'my-add-two-ideographic-spaces-at-bol
-    "mT" #'my-load-default-theme
-    "mt" #'my-load-theme
-    "mx" #'execute-extended-command
-    ;; Org.
-    "o"  #'(:ignore t)
-    "oa" #'org-agenda
-    "ob" #'org-switchb
-    "oc" #'org-capture
-    "ot" #'org-toggle-link-display
-    ;; Search.
-    "s"  #'(:ignore t)
-    "sd" #'search-dired-dwim
-    "sD" #'search-dired
-    "sf" #'consult-fd
-    "sF" #'consult-find
-    "sg" #'consult-grep
-    "sG" #'consult-git-grep
-    "so" #'my-search-online
-    "sr" #'consult-ripgrep
-    "ss" #'consult-line
-    "su" #'consult-focus-lines
-    "sk" #'consult-keep-lines
-    "si" #'imenu
-    ;; Toggle.
-    "t"  #'(:ignore t)
-    "tA" #'abbrev-mode
-    "tD" #'darkroom-mode
-    "ta" #'auto-fill-mode
-    "td" #'darkroom-tentative-mode
-    "tff" #'toggle-frame-fullscreen
-    "tfm" #'toggle-frame-maximized
-    "tg" #'glasses-mode
-    "th" #'global-hl-line-mode
-    "ti" #'display-fill-column-indicator-mode
-    "tj" #'toggle-truncate-lines
-    "tk" #'visual-line-mode
-    "tl" #'display-line-numbers-mode
-    "ts" #'subword-mode
-    "tt" #'load-theme
-    "tv" #'view-mode
-    "tw" #'whitespace-mode
-    ;; Window.
-    "w"   #'(:ignore t)
-    "wd"  #'delete-window
-    "wh"  #'evil-window-left
-    "wj"  #'evil-window-down
-    "wk"  #'evil-window-up
-    "wl"  #'evil-window-right
-    "ws"  #'split-window-below
-    "wv"  #'split-window-right))
 
 (provide 'init-evil)
 
