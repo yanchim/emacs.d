520afd893ff74d04f033325d623ac5b8b1d285fe
yanchim <yanchi.x@foxmail.com>
2024-04-10 09:22:12 +0800

    Update dabbrev-ignored-buffer-modes

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -28,8 +28,9 @@
     :config
     (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
     ;; Since 29.1, use `dabbrev-ignored-buffer-regexps' on older.
+    (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
     (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
-    (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)))
+    (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode)))
 
 (if (display-graphic-p)
     (use-package corfu-popupinfo
